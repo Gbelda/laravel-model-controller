7f9b538b7840d97ca772f3355db9cd0b044676db
gbelda <gbelda56@gmail.com>
2022-01-11 15:30:02 +0100

    Print movies in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,20 @@
 @section('content')
     <h1>Movies</h1>
 
-    <div class="movie">
-        <h3>TITLE</h3>
-        <h4>Original Title</h4>
-        <p>
-            Description
-        </p>
-    </div>
+    @foreach ($movies as $movie)
+
+        <div class="card">
+            <div class="title">
+                <h2>{{ $movie->title }}</h2>
+                <h4>{{ $movie->original_title }}</h5>
+            </div>
+
+            <div class="data">
+                <h5>{{ $movie->nationality }}</h5>
+                <h5>Vote: {{ $movie->vote }}</h5>
+                <h5>Release Year: {{ date('Y', strtotime($movie->date)) }}</h5>
+            </div>
+        </div>
+    @endforeach
+
 @endsection

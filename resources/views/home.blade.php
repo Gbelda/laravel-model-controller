cb031eeb86ac68a9aa1ed476d3f2cfe9de82308b
gbelda <gbelda56@gmail.com>
2022-01-11 15:51:27 +0100

    create scss file + styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Movies</h1>
-
-    @foreach ($movies as $movie)
-
-        <div class="card">
-            <div class="title">
-                <h2>{{ $movie->title }}</h2>
-                <h4>{{ $movie->original_title }}</h5>
-            </div>
-
-            <div class="data">
-                <h5>{{ $movie->nationality }}</h5>
-                <h5>Vote: {{ $movie->vote }}</h5>
-                <h5>Release Year: {{ date('Y', strtotime($movie->date)) }}</h5>
-            </div>
+    <main>
+        <div class="head_title">
+            <h1>Movies</h1>
         </div>
-    @endforeach
+        <div class="content">
+            @foreach ($movies as $movie)
+                <div class="card">
+                    <div class="title">
+                        <h2>{{ $movie->title }}</h2>
+                        <h4>{{ $movie->original_title }}</h5>
+                    </div>
+
+                    <div class="data">
+                        <h5>{{ $movie->nationality }}</h5>
+                        <h5>Vote: {{ $movie->vote }}</h5>
+                        <h5>Release Year: {{ date('Y', strtotime($movie->date)) }}</h5>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </main>
 
 @endsection
